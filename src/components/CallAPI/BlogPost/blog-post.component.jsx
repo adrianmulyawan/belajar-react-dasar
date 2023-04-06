73e90524d8dd78e28f7d366c70516507e65c1abb
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-06 12:57:11 +0700

    Materi delete (hapus data)

--- src/components/CallAPI/BlogPost/blog-post.component.jsx
+++ src/components/CallAPI/BlogPost/blog-post.component.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 
 const BlogPostComponent = () => {
   const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
 
   // > Menggunakan Fetch API
   // const getDataPost = async () => {
@@ -23,6 +25,7 @@ const BlogPostComponent = () => {
     // console.info(item);
     // console.info(item);
     setPosts(item);
+    setLoading(true);
   }
 
   let i = 0;
@@ -33,19 +36,39 @@ const BlogPostComponent = () => {
     }
   }, [i]);
 
+  // > Method Hapus Data
+  const handleRemovePost = async (data) => {
+    console.info(data, 'ini ID');
+    await axios.delete(`http://localhost:3004/posts/${data}`);
+    getDataPost();
+    setMessage('Data Berhasil Dihapus');
+  };
+
   return (
     <>
       <div className="container-fluid mt-3">
         <h1>Blog Post</h1>
+        {/* Alert (Dijalankan ketika data dihapus) */}
+        {
+          message ? ( <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>{ message }</strong>
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div> ) : ''
+        }
+        {/* Dijalankan pada saat pertama kali data di load */}
+        {
+          loading === false ? <p><i>Data Loading</i></p> : ''
+        }
         <div className="card-post">
           <div className="row justify-content-center">
             {
               posts.map((post) => {
                 return <PostCardComponent 
                   key={ post.id } 
-                  title={ post.title }
-                  // author= { post.author }
-                  body={ post.body }
+                  // title={ post.title }
+                  // body={ post.body }
+                  data={ post }
+                  removePost={ handleRemovePost }
                 />
               })
             }
--- src/components/CallAPI/PostCard/post-card.component.jsx
+++ src/components/CallAPI/PostCard/post-card.component.jsx
@@ -1,21 +1,26 @@
 const PostCardComponent = (props) => {
-  const { keygen, title, body, author } = props;
+  const { data, removePost } = props;
+
+  // console.info(data, 'ini data');
+  
   return (
     <>
-      <div className="col-lg-3 col-md-4 col-sm-12 my-2" key={ keygen }>
+      <div className="col-lg-3 col-md-4 col-sm-12 my-2" key={ data.id }>
         <div className="card">
           <img src="https://random.imagecdn.app/640/453" className="card-img-top" alt="banner-img" />
           <div className="card-body">
             <h5 className="card-title">
-              { title }
+              { data.title }
             </h5>
             <p className="text-muted">
               Author: Pablo
             </p>
             <p className="card-text">
-              { body }
+              { data.body }
             </p>
-            <a href="/" className="btn btn-primary float-end">Show Detail</a>
+            <button className="badge bg-danger float-end text-decoration-none px-4 py-2" onClick={ () => removePost(data.id) }>
+              Delete
+            </button>
           </div>
         </div>
       </div>
