df3274071f2ae7e4605239f5c37e4f7398b84f0d
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-07 18:54:41 +0700

    Tambah routing untuk halaman home, product, dan content youtube

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeContainer from './container/Home/home.container';
-// import YoutubeComponent from './components/YoutubeComponent/youtube.component';
-// import HelloComponent from './components/HelloComponent/hello.component';
-// import ClassStatefullComponent from './components/class-statefull.component';
-// import HelloComponent from './components/stateless.component';
+import ProductContainer from './container/Product/product.container';
+import ContentContainer from './container/Content/content.container';
 
 function App() {
   return (
    <>
-    <HomeContainer />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <HomeContainer /> } />
+        <Route path='/product' element={ <ProductContainer /> } />
+        <Route path='/content' element={ <ContentContainer /> } />
+      </Routes>
+    </BrowserRouter>
    </>
   );
 }
