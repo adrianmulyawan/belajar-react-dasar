1d33af81082424f813fd8713c38edffdc8ef4882
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 11:18:20 +0700

    import counter component di card product

--- src/components/CardProductChild/card-product.component.jsx
+++ src/components/CardProductChild/card-product.component.jsx
@@ -1,44 +1,7 @@
-import { useState } from 'react';
 import '../ProductParent/product.component.css';
+import CounterComponent from './counter.component';
 
 const CardProductComponent = (props) => {
-  const [order, setOrder] = useState({
-    count: 1,
-    productName: 'Ayam Boiler 1 KG',
-    productPrice: 56000
-  });
-
-  const handleCounterChange = (newValue) => {
-    props.onCounterChange(newValue);
-  };
-
-  const handleMinus = () => {
-    if (order.count <= 0) {
-      alert('Tidak Bisa Mengurangi Stock!');
-      return false;
-    }
-    setOrder(data => {
-      return {
-        ...data,
-        count: data.count - 1,
-        handleCounterChange: handleCounterChange(data.count - 1)
-      }
-    });
-    console.info('kurang di card product', order.count);
-  };
-
-  const handlePlus = () => {
-    setOrder(data => {
-      // console.info(order.count);
-      return {
-        ...data,
-        count: data.count + 1,
-        handleCounterChange: handleCounterChange(data.count + 1)
-      }
-    });
-    console.info('tambah di card product', order.count);
-  };
-
   return (
     <>
       <div className="card-product"> 
@@ -46,16 +9,12 @@ const CardProductComponent = (props) => {
           <img src="https://images.tokopedia.net/img/cache/500-square/product-1/2020/8/22/101342057/101342057_68f4d174-988b-41b7-98e8-3e03a5cfb45e_700_700" alt="img-product" />
         </div>
         <p className="product title">
-          { order.productName }
+          Ayam Boiler 1 Kg
         </p>
         <p className="product-price">
-          Rp { order.productPrice }
+          Rp 35.000,00
         </p>
-        <div className="counter">
-          <button className="minus" onClick={ () => handleMinus() }>-</button>
-          <input type="text" name="quantity" id="quantity" value={ order.count } disabled />
-          <button className="plus" onClick={ () => handlePlus() }>+</button>
-        </div>
+        <CounterComponent onCounterChange={(value) => props.onCounterChange(value)} />
       </div>
     </>
   )
